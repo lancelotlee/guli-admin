3c62f6d2b0c493953b183c231e325f4021ec62d5
lancelotlee <sorlinlee93@gmail.com>
2020-08-03 14:58:03 +0800

    课程信息

---
+++ src/api/edu/subject.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+export default {
+  getAllSubject() {
+    return request({
+      url: `/eduservice/edu-subject/getAllSubject`,
+      method: 'get'
+    })
+  }
+}
